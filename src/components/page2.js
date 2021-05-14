758a684b693780eba1ee10b1e42e391d6a485a54
googoo2485 <googoo2485@github.com>
2021-05-14 16:01:06 +0900

    no message

---
+++ src/components/page2.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { ButtonBack, ButtonNext } from 'pure-react-carousel';
+
+
+
+
+
+
+
+
+
+
+
+
+
+const page2 = () => {
+    return (
+        <div className="page-2">
+            <ButtonNext></ButtonNext>
+        </div>
+    )
+}
+
+export default page2
---
+++ src/components/page3.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { ButtonBack, ButtonNext } from 'pure-react-carousel';
+const page3 = () => {
+    return (
+        <div className="page-3">
+            
+        </div>
+    )
+}
+
+export default page3
