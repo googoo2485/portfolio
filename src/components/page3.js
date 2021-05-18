dcbbcb3d9957c40281012e6acc1e050a1f14aa09
googoo2485 <googoo2485@github.com>
2021-05-18 11:59:19 +0900

    no message

--- src/components/page3.js
+++ src/components/page3.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ButtonBack, ButtonNext } from 'pure-react-carousel';
+import Aimg from '../img/8ver.png';
 const page3 = () => {
     return (
         <div className="page-3">
@@ -8,38 +9,26 @@ const page3 = () => {
         </ButtonBack>
         <div className="container-fluid">
           <div className="title-wrapper">
-            <h2 className="project-title-p3">Bond</h2>
-            <h4 className="sub-title">그룹 기반 SNS</h4>
+            <h2 className="project-title-p3">8ver</h2>
+            <h4 className="sub-title">블록체인 커뮤니티</h4>
           </div>
           <div className="content-wrapper row">
             <div className="col-md-6 video-wrapper">
               <div className="embed-responsive embed-responsive-16by9 media">
-                <iframe title="project1" className="embed-responsive-item project-video" src="https://www.youtube.com/embed/gty0xzSBzRk" frameBorder="0" gesture="media" allowFullScreen />
+                <img title="project1" className="embed-responsive-item" src={Aimg}/>
               </div>
               <div className="btn-wrapper">
                 <div className="btn-group" role="group" aria-label="button group">
-                  <form action="https://github.com/realkth/Bond" target="_blank">
+                  <form action="https://github.com/googoo2485/8teamProject" target="_blank">
                     <button type="submit" className="btn btn-info-link btn-outline-secondary">
                       <span className="fa fa-github" aria-hidden="true"></span>
                       &nbsp;Github
                   </button>
                   </form>
-                  <form action="http://bond.thekym.com" target="_blank">
+                  <form action="http://tkdrn2485.dothome.co.kr/" target="_blank">
                     <button type="submit" className="btn btn-info-link btn-outline-secondary">
                       <span className="fa fa-globe" aria-hidden="true"></span>
                       &nbsp;Web Site
-                  </button>
-                  </form>
-                  <form action="https://docs.google.com/spreadsheets/d/19uk0yIWIGAGDWxCyIE3sBjMY3eeNxImAGhN5P_W6S3M/edit?usp=sharing" target="_blank">
-                    <button type="submit" className="btn btn-info-link btn-outline-secondary">
-                      <span className="fa fa-pencil-square-o" aria-hidden="true"></span>
-                      &nbsp;Daily Scrum
-                  </button>
-                  </form>
-                  <form action="https://youtu.be/gty0xzSBzRk" target="_blank">
-                    <button type="submit" className="btn btn-info-link btn-outline-secondary">
-                      <span className="fa fa-youtube-play" aria-hidden="true"></span>
-                      &nbsp;Youtube
                   </button>
                   </form>
                 </div>
@@ -49,20 +38,21 @@ const page3 = () => {
               <ul className="info">
                 <li className="info-list">
                   <h5 className="content-title"><span className="fa fa-hashtag" aria-hidden="true"></span> 개요</h5>
-                  <p className="content-text-summary fa fa-angle-right" aria-hidden="true"> 패스트캠퍼스 프론트엔드 개발 스쿨 수료 프로젝트로 백엔드 개발 스쿨 수료생과 함께 네이버 Band를 모티브로 그룹 생성과 팔로우 기능이 있는 SNS서비스 제작
+                  <p className="content-text-summary fa fa-angle-right" aria-hidden="true"> 블록체인 암호화폐를 주제로 한 커뮤니티 사이트 제작
                     </p>
                 </li>
                 <li className="info-list">
                   <h5 className="content-title"><span className="fa fa-hashtag" aria-hidden="true"></span> 주요 역할</h5>
                   <ul className="content-text-wil">
-                    <li className="fa fa-check" aria-hidden="true"> Vue.js와 Scss를 활용한 프론트엔드 작업</li><br />
-                    <li className="fa fa-check" aria-hidden="true"> 백엔드 개발자와의 협업</li><br />
-                    <li className="fa fa-check" aria-hidden="true"> Axios를 이용한 데이터 통신</li><br />
-                    <li className="fa fa-check" aria-hidden="true"> 스크럼보드와 깃헙 프로젝트, 이슈 메뉴를 활용한 프로젝트 일정 관리</li>
+                    <li className="fa fa-check" aria-hidden="true"> php와 js,Mysql을 이용하여 작업</li><br/>
+                    <li className="fa fa-check" aria-hidden="true"> 소스트리를 활용하여 협업</li><br />
+                    <li className="fa fa-check" aria-hidden="true"> 로그인,회원가입</li><br />
+                    <li className="fa fa-check" aria-hidden="true"> 게시글 등록,수정,삭제</li><br/>
+                    <li className="fa fa-check" aria-hidden="true"> 게시글 페이징기능 구현</li><br/>
                   </ul>
                 </li>
                 <h5 className="content-title"><span className="fa fa-hashtag" aria-hidden="true"></span> 활용 기술</h5>
-                <p className="content-text-stacks fa fa-angle-right" aria-hidden="true"> <code>Vue.js</code> <code>Axios</code> <code>Bulma</code> <code>Sass</code> 
+                <p className="content-text-stacks fa fa-angle-right" aria-hidden="true"> <code>PHP</code> <code>Mysql</code> <code>JavaScript</code> <code>css</code> 
                 </p>
               </ul>
             </div>
